c09a6c7b0aa1364eec60b8b499eda83d528b9bf5
Shrey <shrym.95@gmail.com>
2016-07-01 20:40:16 +0530

    Design Update

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -49,6 +49,7 @@ $(document).ready(function(){
 		 //Prints in browsers console
 		 	console.log("Shit:" + msg);
 		 	$("#"+msg).hide();
+		 	//$("#"+msg).css('background-color','red'); 
      	 });
     
 	
